533bbeca7e98d789dfb40e86dd5059831df2f0e8
Steven Brus <sbrus@login1.stampede.tacc.utexas.edu>
2015-07-08 09:33:42 -0500

    Update run_cases.py and run_script.py for TACC

--- run_cases.py
+++ run_cases.py
@@ -2,26 +2,29 @@
 
 input_name = 'dgswe.inp'
 
-processors = '24'
+processors = '32'
 prep_time = '00:20:00'
-run_time = ['48:00:00','48:00:00','48:00:00']
+run_time = ['06:00:00','06:00:00','06:00:00']
 
 allocation = 'TG-DMS080016N'
 
-#run_queue = 'normal'
-run_queue = 'athos'
-#prep_queue = 'serial'
-prep_queue = 'proteus'
+run_queue = 'normal'
+#run_queue = 'athos'
+prep_queue = 'serial'
+#prep_queue = 'proteus'
 
-exe_name = 'dgswe'
+exe_name = ['pdgswe_rk22','pdgswe_rk33','pdgswe_rk45']
 prep_name = 'dgprep'
 post_name = 'dgpost'
 
-grid_names = ['test']
+grid_names = ['converge3']
 p_orders = ['1','2','3']
 ctp_orders = ['2','2','3']
 hpb_orders = ['1','2','3']
-timesteps = ['.198d0','.0625d0','.0197d0']
+#timesteps = ['.5d0','.25d0','.125d0']
+#timesteps = ['.25d0','.125d0','.0625d0']
+#timesteps = ['.125d0','.0625d0','.03125d0']
+timesteps = ['.0625d0','.03125d0','.015625d0']
 
 final_time = '1d0'
 ramp_length = '.08d0'
@@ -31,9 +34,9 @@ output_direc = './'
 num_partitions = '1'
 
 # Run, grid, and executable paths
-run_path = '/home/sbrus/SmallProjects/run_script/'
-code_path = '/home/sbrus/Codes/dgswe/work/'
-grid_path = '/home/sbrus/Codes/dgswe/grids/'
+run_path = '/work/01964/sbrus/dgswe_converge_bath_dt2/'
+code_path = '/home1/01964/sbrus/dgswe/work/'
+grid_path = '/home1/01964/sbrus/dgswe/grids/'
 
 # set up cases list
 cases = []
@@ -41,6 +44,7 @@ for k,grid in enumerate(grid_names):
   for i,p in enumerate(p_orders):
     ctp = ctp_orders[i]
     dt = timesteps[i]
+    exe = exe_name[i]
     rtime = run_time[i]
     for j in range(0,i+1):
       hbp = str(j+1)
@@ -60,7 +64,7 @@ for k,grid in enumerate(grid_names):
                     
                     'direc':directory_name,                    
                     'proc':processors,
-                    'exe':code_path+exe_name,
+                    'exe':code_path+exe,
                     'rtime':rtime,
                     'rqueue':run_queue.lower(),
                     
