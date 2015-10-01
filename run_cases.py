4367ef16590c06414f35fad38054abb7a04c1095
Steven Brus <sbrus@nd.edu>
2015-10-01 15:27:49 -0400

    Set up run_cases for converging channel convergence study

--- run_cases.py
+++ run_cases.py
@@ -7,10 +7,10 @@
 #bundle_flag = False          # bundle runs with same number of processors into same submission script
                              ## 'rdirec' needs to be the same for each bundle of runs
 #processors = ['24','48','96','192','384','768']
-##processors = ['24']
+##processors = ['96','192','384','768']
 #prep_time = '00:20:00'
 #run_time = ['01:00:00','01:00:00','01:00:00','01:00:00','01:00:00','01:00:00']
-#max_proc = 768
+##max_proc = 800
 
 #allocation = 'TG-DMS080016N'
 
@@ -26,22 +26,27 @@
 
 
 #grid_names = ['inlet3']
+##grid_names = ['inlet3']
 #p_orders = ['1','2','3']
 #ctp_orders = ['1']
 #hbp_orders = ['1']
 #rk_types = ['22']
 #timesteps = ['.1d0','.1d0','.1d0']
+##timesteps = ['.01d0','.01d0','.01d0']
+
 
 #final_time = '1d0'
 #ramp_length = '.5d0'
 #fric_coef = '.003d0'
 #num_snaps = '10d0'
+##num_snaps = '1d0'
 #output_direc = './'
 #num_partitions = '1'
 
 ## Run, grid, and executable paths
 ##run_path = '/home2/sbrus/dgswe_aegeaon_scaling/'
 #run_path = '/home2/sbrus/dgswem_structure_aegeaon_scaling/'
+##code_path = '/home2/sbrus/dgswe_aegeaon_scaling/inlet6/'
 #code_path = '/home2/sbrus/dgswe/work/'
 #grid_path = '/home2/sbrus/dgswe/grids/'
 
@@ -100,7 +105,9 @@
 
 
 
-
+#############################
+## Inlet convergence study
+#############################
 
 
 
@@ -201,100 +208,99 @@
 
 
 
+####################################################################
+## Converging channel (with high-order bathymetry) convergence study
+####################################################################
+
+# run variables
+
+input_name = 'dgswe.inp'
+
+bundle_flag = False          # bundle runs with same number of processors into same submission script
+                             # 'rdirec' needs to be the same for each bundle of runs
+processors = ['16']
+prep_time = '00:20:00'
+run_time = ['06:00:00','06:00:00','06:00:00']
+max_proc = 800
+
+allocation = 'TG-DMS080016N'
+
+run_queue = 'aegaeon'
+prep_queue = 'proteus'
 
 
-## run variables
+exe_name = 'dgswe_mpi'
+prep_name = 'dgprep'
+post_name = 'dgpost'
 
-#input_name = 'dgswe.inp'
+grid_names = ['converge']
+p_orders = ['1','2','3']
+ctp_orders = ['2','2','3']
+hbp_orders = ['1','2','3']
+rk_types = ['22','33','45']
+timesteps = ['.5d0','.25d0','.125d0']
+#timesteps = ['.25d0','.125d0','.0625d0']
+#timesteps = ['.125d0','.0625d0','.03125d0']
+#timesteps = ['.0625d0','.03125d0','.015625d0']
 
-#bundle_flag = False          # bundle runs with same number of processors into same submission script
-                             ## 'rdirec' needs to be the same for each bundle of runs
-#processors = ['32']
-#prep_time = '00:20:00'
-#run_time = ['06:00:00','06:00:00','06:00:00']
-#max_proc = 768
+final_time = '1d0'
+ramp_length = '.08d0'
+fric_coef = '.0025d0'
+num_snaps = '10d0'
+output_direc = './'
+num_partitions = '1'
 
-#allocation = 'TG-DMS080016N'
+# Run, grid, and executable paths
+run_path = '/home2/sbrus/dgswe_converge_bath/'
+code_path = '/home2/sbrus/dgswe/work/'
+grid_path = '/home2/sbrus/dgswe/grids/'
+#run_path = '/work/01964/sbrus/dgswe_converge_bath_dt2/'
+#code_path = '/home1/01964/sbrus/dgswe/work/'
+#grid_path = '/home1/01964/sbrus/dgswe/grids/'
 
-#run_queue = 'normal'
-##run_queue = 'athos'
-#prep_queue = 'serial'
-##prep_queue = 'proteus'
-
-#exe_name = 'dgswe'
-##exe_name = 'pdgswe'
-#prep_name = 'dgprep'
-#post_name = 'dgpost'
-
-#grid_names = ['converge']
-#p_orders = ['1','2','3']
-#ctp_orders = ['2','2','3']
-#hbp_orders = ['1','2','3']
-#rk_types = ['22','33','45']
-#timesteps = ['.5d0','.25d0','.125d0']
-##timesteps = ['.25d0','.125d0','.0625d0']
-##timesteps = ['.125d0','.0625d0','.03125d0']
-##timesteps = ['.0625d0','.03125d0','.015625d0']
-
-#final_time = '1d0'
-#ramp_length = '.08d0'
-#fric_coef = '.0025d0'
-#num_snaps = '10d0'
-#output_direc = './'
-#num_partitions = '1'
-
-## Run, grid, and executable paths
-#run_path = '/home/sbrus/SmallProjects/run_script/test/'
-#code_path = '/home/sbrus/Codes/dgswe/work/'
-#grid_path = '/home/sbrus/Codes/dgswe/grids/'
-##run_path = '/work/01964/sbrus/dgswe_converge_bath_dt2/'
-##code_path = '/home1/01964/sbrus/dgswe/work/'
-##grid_path = '/home1/01964/sbrus/dgswe/grids/'
-
-## set up cases list
-#cases = []
-#for k,grid in enumerate(grid_names):
-  #for i,p in enumerate(p_orders):
-    #ctp = ctp_orders[i]
-    #dt = timesteps[i]
-    #rk = rk_types[i]
-    #rtime = run_time[i]
-    #for j in range(0,i+1):
-      #hbp = str(j+1)
-      #directory_name = run_path + grid+'/' + 'p'+p+'/' + 'ctp'+ctp+'/' + 'hbp'+hbp+'/'
-      #cases.append({'input':directory_name+input_name,
-	            #'mesh':grid_path+grid ,
-	            #'copy_files':[grid_path+grid+"_hbp"+hbp+".d"]
+# set up cases list
+cases = []
+for k,grid in enumerate(grid_names):
+  for i,p in enumerate(p_orders):
+    ctp = ctp_orders[i]
+    dt = timesteps[i]
+    rk = rk_types[i]
+    rtime = run_time[i]
+    for j in range(0,i+1):
+      hbp = str(j+1)
+      directory_name = run_path + grid+'/' + 'p'+p+'/' + 'ctp'+ctp+'/' + 'hbp'+hbp+'/'
+      cases.append({'input':directory_name+input_name,
+	            'mesh':grid_path+grid ,
 	            
-                    #'p':p,
-                    #'ctp':ctp, 
-                    #'hbp':hbp, 
+                    'p':p,
+                    'ctp':ctp, 
+                    'hbp':hbp, 
                     
-                    #'rk':rk,
-                    #'dt':dt,                     
-                    #'tf':final_time,
-                    #'dramp':ramp_length,
+                    'rk':rk,
+                    'dt':dt,                     
+                    'tf':final_time,
+                    'dramp':ramp_length,
                     
-                    #'cf':fric_coef, 
+                    'cf':fric_coef, 
                     
-                    #'nlines':num_snaps,
-                    #'outdir':output_direc,
+                    'nlines':num_snaps,
+                    'outdir':output_direc,
                     
-                    #'npart':num_partitions, 
+                    'npart':num_partitions, 
                     
-                    #'direc':directory_name,    
-                    ##'rdirec':run_path,
-                    #'rdirec':directory_name,
-                    #'proc':processors[0],
-                    #'exe':code_path+exe_name,
-                    #'rtime':rtime,
-                    #'rqueue':run_queue.lower(),
+                    'direc':directory_name,    
+                    #'rdirec':run_path,
+                    'rdirec':directory_name,
+                    'proc':processors[0],
+                    'exe':code_path+exe_name,
+                    'rtime':rtime,
+                    'rqueue':run_queue.lower(),
                     
-                    #'prep':code_path+prep_name,
-                    #'ptime':prep_time,
-                    #'pqueue':prep_queue.lower(),
-                    #'alloc':allocation,
-                    #'post':code_path+post_name})
+                    'prep':code_path+prep_name,
+                    'ptime':prep_time,
+                    'pqueue':prep_queue.lower(),
+                    'alloc':allocation,
+                    'post':code_path+post_name})
 
                     
                     
@@ -343,93 +349,3 @@
 
 
 
-
-
-
-
-
-# run variables
-
-input_name = 'dgswe.inp'
-
-bundle_flag = False          # bundle runs with same number of processors into same submission script
-                             # 'rdirec' needs to be the same for each bundle of runs
-processors = ['96','192','384','768']
-prep_time = '00:20:00'
-run_time = ['06:00:00','03:00:00','01:30:00','01:30:00','01:30:00']
-max_proc = 800
-
-allocation = 'TG-DMS080016N'
-
-#run_queue = 'normal'
-run_queue = 'aegaeon'
-#prep_queue = 'serial'
-prep_queue = 'proteus'
-
-exe_name = 'dgswe_mpi'
-prep_name = 'dgprep'
-post_name = 'dgpost_nc'
-
-grid_names = ['inlet6']
-p_orders = ['1','2','3']
-ctp_orders = ['1']
-hbp_orders = ['1']
-rk_types = ['22']
-timesteps = ['.01d0','.01d0','.01d0']
-
-final_time = '.1d0'
-ramp_length = '.5d0'
-fric_coef = '.003d0'
-num_snaps = '1d0'
-output_direc = './'
-num_partitions = '1'
-
-# Run, grid, and executable paths
-run_path = '/home2/sbrus/dgswe_aegeaon_scaling/'
-code_path = '/home2/sbrus/dgswe_aegeaon_scaling/inlet6/'
-grid_path = '/home2/sbrus/dgswe/grids/'
-
-# set up cases list
-cases = []
-for k,cores in enumerate(processors):
-  rtime = run_time[k]  
-  for i,p in enumerate(p_orders):
-    ctp = ctp_orders[0]
-    hbp = hbp_orders[0]
-    dt = timesteps[i]
-    rk = rk_types[0]
-    grid = grid_names[0]
-
-    directory_name = run_path + grid+'/' + 'p'+p+'/' + 'np'+cores+'/'
-    cases.append({'input':directory_name+input_name,
-                    'mesh':grid_path+grid ,
-                    
-                    'p':p,
-                    'ctp':ctp, 
-                    'hbp':hbp, 
-                    
-                    'rk':rk,
-                    'dt':dt,                     
-                    'tf':final_time,
-                    'dramp':ramp_length,
-                    
-                    'cf':fric_coef, 
-                    
-                    'nlines':num_snaps,
-                    'outdir':output_direc,
-                    
-                    'npart':num_partitions, 
-                    
-                    'direc':directory_name,   
-                    'rdirec':directory_name,
-                    #'rdirec':run_path,
-                    'proc':cores,
-                    'exe':code_path+exe_name,
-                    'rtime':rtime,
-                    'rqueue':run_queue.lower(),
-                    
-                    'prep':code_path+prep_name,
-                    'ptime':prep_time,
-                    'pqueue':prep_queue.lower(),
-                    'alloc':allocation,
-                    'post':code_path+post_name})
